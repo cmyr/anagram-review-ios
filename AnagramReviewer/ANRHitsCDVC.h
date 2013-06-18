04cfcfac7ed885fb5e8606f100772915ac921b2c
Colin Rothfels <colin@cmyr.net>
2013-06-17 21:54:17 -0400

    added core data table view controller, to be implemented

---
+++ AnagramReviewer/ANRHitsCDVC.h
@@ -0,0 +1,13 @@
+//
+//  ANRHitsCDVC.h
+//  AnagramReviewer
+//
+//  Created by Colin Rothfels on 2013-06-17.
+//  Copyright (c) 2013 cmyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANRHitsCDVC : UITableViewController
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@end
